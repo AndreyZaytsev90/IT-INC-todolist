bf62aef48bac97d41026e214cdccf417f81033fe
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-27 20:55:17 +0300

    change files

--- todolist/ts-04/src/TodoList.tsx
+++ todolist/ts-04/src/TodoList.tsx
@@ -13,12 +13,14 @@ type TodoListPropsType = {
 
 const TodoList = (props: TodoListPropsType) => {
     const [title, setTitle] = useState<string>("")
+    const [error, setError] = useState<boolean>(false)
     const tasksList = props.tasks.map((task: TaskType) => { //tasks - массив, task - элемент массива
         const removeTask = () => props.removeTask(task.id)
 
         const changeStatus = (event: ChangeEvent<HTMLInputElement>) => props.changeTaskStatus(task.id, event.currentTarget.checked) // объект, где содержится сведения о произошедшем событии
 
         return (
+            //Затемнение
             <li key={task.id} className={task.isDone ? "is-done" : "" }>
                 <input
                     type="checkbox"
@@ -34,6 +36,8 @@ const TodoList = (props: TodoListPropsType) => {
         const trimmedTitle = title.trim() // обрезает пробелы по краям названия таски
         if (trimmedTitle) {
             props.addTask(trimmedTitle)
+        } else {
+            setError(true)
         }
         setTitle("")
     }
@@ -42,28 +46,49 @@ const TodoList = (props: TodoListPropsType) => {
                 addTask()
             }
     }
-    const onClickChangeTitle = (event: ChangeEvent<HTMLInputElement>) => setTitle(event.currentTarget.value)
+    const onChangeSetTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+        setError(false)
+    }
     const onClickSetAllFilter = () => props.changeFilter("all")
     const onClickSetActiveFilter = () => props.changeFilter("active")
     const onClickSetCompletedFilter = () => props.changeFilter("completed")
+
+
+
+
+    const allBtnClasses = props.filter === "all" ? "active-filter": "" //активность кнопки
+    const getBtnClass = (filter: FilterValuesType) => {     // Тоже самое что и сверху. Активность кнопки . Снизу третий вариант!
+      return props.filter === filter ? "active-filter": ""
+       // return ["btn btn-key"].concat(filter).join(" ")
+    }
+
+    const errorStyle = {color: "red"}
+    const errorMessage = error
+        ? <div style={errorStyle}>Title is required!</div>
+        : <div>Enter task title</div>
+
+
 
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
                 <input
+                    className={error ? "error" : ""}
                     value={title}
-                    onChange={onClickChangeTitle}
+                    onChange={onChangeSetTitle}
                     onKeyPress={onKeyPressAddTask} // = (event) => onKeyPressAddTask(event)
                 />
                 <button onClick={addTask}>+</button>
+                {errorMessage}
             </div>
             <ul>
                 {tasksList}
             </ul>
             <div>
-                <button className={props.filter === "all" ? "active-filter": ""} onClick={onClickSetAllFilter}>All</button>
-                <button className={props.filter === "active" ? "active-filter": ""} onClick={onClickSetActiveFilter}>Active</button>
+                <button className={allBtnClasses} onClick={onClickSetAllFilter}>All</button>
+                <button className={getBtnClass("active") } onClick={onClickSetActiveFilter}>Active</button>
                 <button className={props.filter === "completed" ? "active-filter": ""} onClick={onClickSetCompletedFilter}>Completed</button>
             </div>
         </div>
