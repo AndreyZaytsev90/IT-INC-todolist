45b1ba8970bb884257bf27c8fdc59bf974ecddae
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-06 22:48:21 +0300

    Change files

--- todolist(Igor)/ts-01/todolist/src/App.tsx
+++ todolist(Igor)/ts-01/todolist/src/App.tsx
@@ -5,8 +5,8 @@ import {Todolist} from "./Todolist";
 function App() {
     return (
         <div className="App">
-            <Todolist title = {"What to learn"} title2 = {"in React"}/>
-            <Todolist title = {"What to buy"} title2 = {"in shop"}/>
+            <Todolist title = {"What to learn"} />
+            <Todolist title = {"What to buy"} />
         </div>
     )
 }
--- todolist(Igor)/ts-01/todolist/src/Todolist.tsx
+++ todolist(Igor)/ts-01/todolist/src/Todolist.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 type PropsType = {
     title: string
-    title2: string
+    title2?: string  // ? - значит что пропс может и не прийти.
 }
 
 
@@ -10,7 +10,7 @@ export const Todolist = (props: PropsType) => {
     return (
         <div>
             <h3>{props.title}</h3>
-            <h2>{props.title2}</h2>
+            <h5>{props.title2}</h5>
             <div>
                 <input/>
                 <button>+</button>
