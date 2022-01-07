756d722e9c2bb01ea7d10dbddfde043b85531da9
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-07 21:02:56 +0300

    Change file

--- todolist(Igor)/ts-01/todolist/src/Todolist.tsx
+++ todolist(Igor)/ts-01/todolist/src/Todolist.tsx
@@ -13,6 +13,7 @@ type inArray = {
 }
 
 export const Todolist = (props: PropsType) => {
+
     return (
         <div>
             <h3>{props.title}</h3>
@@ -21,9 +22,15 @@ export const Todolist = (props: PropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
+                {props.task.map(item => { // item - один обьект из массива
+                    debugger
+                    return (
+                        <li key={item.id}><input type="checkbox" checked={item.isDone}/> <span>{item.title}</span></li>
+                    )
+                })}
+               {/* <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
                 <li><input type="checkbox" checked={props.task[1].isDone}/> <span>{props.task[1].title}</span></li>
-                <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>
+                <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>*/}
             </ul>
             <div>
                 <button>All</button>
