43d40b56b664516e9668baea7af70bd9cb0e6026
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-10 22:20:08 +0300

    change file

--- todolist(Igor)/ts-01/todolist/src/App.tsx
+++ todolist(Igor)/ts-01/todolist/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
 
-    const tasks1 = [
+    let [tasks, setTasks] = useState( [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false }
-    ]
-    
+    ])//rcv123 - ячейка памяти
     const removeTask = (id: number) => {
-
-        console.log("crecr")
+        tasks = tasks.filter(value=>value.id !== id)  //rcv124 ячейка памяти изменилась с помощью setTasks
+        setTasks(tasks)
     }
-
+   /* useState = массив
+        [tasks, setTasks] - tasks - свойство, setTasks - метод*/
     return (
         <div className="App">
             <Todolist title="What to learn"
-                      tasks={tasks1}
+                      tasks={tasks}
                       removeTask={removeTask}
             />
         </div>
