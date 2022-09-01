ac974b0e860f7a1bc5f442541b6b785ac4ec1b65
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-01 14:49:45 +0300

    Changed file. Added types

--- todo/src/api/task-api.ts
+++ todo/src/api/task-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const todolistId1 = 'faa061cc-fde7-41f6-955e-3eda62b445d4'
+const todolistId1 = '60f64e2e-eb47-4c32-bcf2-90b585289b39'
 const todolistId2 = '469c51c1-6078-4684-93a9-2a6d94d28648'
 const todolistId3 = 'b89d8b6b-acb7-465c-8a85-a17592753c75'
 
@@ -13,20 +13,40 @@ const instance = axios.create({
   },
 })
 
+export type ResponseTaskType<T> = {
+  data: T;
+  messages: Array<string>;
+  fieldsErrors: Array<string>;
+  resultCode: number;
+}
+
+export type T = {
+  item: TaskType;
+}
+
+export type TaskType = {
+  id: string;
+  title: string;
+  todoListId: string;
+  order: number;
+  status: number;
+  priority: number;
+  addedDate: Date
+}
 
 export const taskAPI = {
   getTasks() {
-    const promise = instance.get('tasks')
+    const promise = instance.get<Array<TaskType>>('tasks')
     return promise
   },
   createTask(title: string) {
-    return instance.post('tasks', {title: title})
+    return instance.post<ResponseTaskType<T>>('tasks', {title: title})
   },
   deleteTask(taskId: string) {
-    return instance.delete(`tasks/${taskId}`)
+    return instance.delete<ResponseTaskType<{}>>(`tasks/${taskId}`)
   },
   updateTask(taskId: string, title: string) {
-    return instance.put(`tasks/${taskId}`, {title: title})
+    return instance.put<ResponseTaskType<{}>>(`tasks/${taskId}`, {title: title})
   },
 
 }
